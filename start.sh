78bea9d45e83693edb72cf762ad391ceba4e861e
a.walker <a.walker@adito.de>
2018-11-15 07:38:10 +0100

    changed position of copy command

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 if [ ! -f /ssh/ssh_host_rsa_key ]
   then
-    ssh-keygen -t rsa -b 4096 -f /ssh/ssh_host_rsa_key -N ''
+    ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ''
+  else 
+    cp /ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
+    cp /ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa_key.pub
 fi
 
 echo "ForceCommand echo this account can only be used for tunneling" > /etc/ssh/sshd_config
 echo "HostKey /ssh/ssh_host_rsa_key" >> /etc/ssh/sshd_config
 echo "PermitOpen ${SSH_PERMITOPEN}" >> /etc/ssh/sshd_config
 echo "AllowUsers ${SSH_USER}" >> /etc/ssh/sshd_config
-
-cp /ssh/ssh_host_rsa_key /etc/ssh/ssh_host_rsa_key
-cp /ssh/ssh_host_rsa_key.pub /etc/ssh/ssh_host_rsa_key.pub
 
 chmod 400 /etc/ssh/*
 chown root:root /etc/ssh/*
